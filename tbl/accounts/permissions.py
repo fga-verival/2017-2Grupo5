570218f4036489933420e2fd6e4acf0a47a03727
Victor Arnaud <victorhad@gmail.com>
2017-07-27 04:10:17 -0300

    Finish authentication and create token

--- tbl/accounts/permissions.py
+++ tbl/accounts/permissions.py
@@ -18,17 +18,3 @@ class UpdateOwnProfile(BasePermission):
         # It will check if the profile ID that they're trying to update
         # is the authenticated user profile, their own profile.
         return obj.id == request.user.id
-
-
-class IsOwnerOrReadOnly(BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Read permission are allowed to any request.
-        if request.method in SAFE_METHODS:
-            return True
-
-        # Write permission are only allowed to the owner.
-        return obj.owner == request.user
--- tbl/accounts/serializers.py
+++ tbl/accounts/serializers.py
@@ -1,10 +1,8 @@
 from rest_framework.serializers import (
     ModelSerializer,
-    EmailField,
     CharField,
     ValidationError
 )
-from uuid import uuid4
 from .models import User
 
 
@@ -23,16 +21,29 @@ class UserRegisterSerializer(ModelSerializer):
     A serializer to register a new user.
     """
 
+    confirm_password = CharField()
+
     class Meta:
         model = User
-        fields = ['email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['name', 'email', 'password', 'confirm_password']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'confirm_password': {'write_only': True}
+        }
 
     def validate(self, data):
         email = data['email']
+        password = data['password']
+        confirm_password = data['confirm_password']
+
+        # Verify if exists another user with same email address
         user = User.objects.filter(email=email)
         if user.exists():
             raise ValidationError('This user has already registered.')
+
+        # Verify if the passwords not match.
+        if password != confirm_password:
+            raise ValidationError('The passwords do not match.')
 
         return data
 
@@ -41,48 +52,11 @@ class UserRegisterSerializer(ModelSerializer):
         Create and return a new user.
         """
 
-        user = User(email=validated_data['email'])
+        user = User(
+            email=validated_data['email'],
+            name=validated_data['name']
+        )
         user.set_password(validated_data['password'])
         user.save()
 
         return validated_data
-
-
-class UserLoginSerializer(ModelSerializer):
-    """
-    A serializer to authentication login.
-    """
-
-    token = CharField(allow_blank=True, read_only=True)
-    email = EmailField(label='Email address', allow_blank=True, required=False)
-
-    class Meta:
-        model = User
-        fields = ['email', 'password', 'token']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def validate(self, data):
-        user = None
-        password = data['password']
-
-        # Verify if email is empty
-        email = data.get('email', None)
-        if not email:
-            raise ValidationError('The email is required to login.')
-
-        # Verify if exists another user with the same email address
-        users = User.objects.filter(email=email)
-        if users.exists() and users.count() == 1:
-            user = users.first()
-        else:
-            raise ValidationError('The email is not valid!')
-
-        # Verify if the password inserted is correct.
-        if user:
-            if not user.check_password(password):
-                raise ValidationError('Invalid credentials, please try again')
-
-        # Creates a unique token every time it's called
-        data['token'] = uuid4()
-
-        return data
